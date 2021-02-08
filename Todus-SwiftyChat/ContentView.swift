b75cd6927dce12db79f0d35424320d80d4b5579e
wilderlopez <wlopezelias@gmail.com>
2021-02-08 14:54:11 -0500

    Initial Commit

---
+++ Todus-SwiftyChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todus-SwiftyChat
+//
+//  Created by Wilder Lopez on 2/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
